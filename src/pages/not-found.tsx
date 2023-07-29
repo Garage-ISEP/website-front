010ed76cd7326b8dd6ac07d0f98269e79003efba
MaxxxxxxLh <122477345+MaxxxxxxLh@users.noreply.github.com>
2023-07-29 22:11:42 +0200

    .

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 
 
+
 const PageNotFound= () =>{
     return(
-        <div style={{ backgroundImage: "url(/mascotteIsep.jpg)" }}>
-            <h1>Désolé, Garage n'a pas réussi à trouver ta page!!</h1>
-            <p>Voici un lien pour retourner à l'accueil</p> 
-            <a href='/'>Page d'accueil</a>
-            <p></p>
-            <a href='/silicon-days'>Silicon days</a>
+        <div className="grid grid-cols-3 gap-4 items-center">
+                <div className="block text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
+                    <h1 className="text-end">Désolé, Garage n'a pas réussi à trouver ta page!!</h1>
+                </div>
+                <div className="justify-self-center">
+                    <img  src="Garage-logo.svg"></img>
+                </div>
+                <ul className="list-inside list-disc text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
+                    <h1 className="underline font-bold">Différents liens:</h1>
+                    <li className="underline"><a href='/' className="inline-block">Page d'accueil</a></li>
+                    <li className="underline"><a href='/silicon-days' className="inline-block">Silicon days</a></li>
+                </ul>
+            
         </div>
-        
     )
 }
 
 export default PageNotFound
+
