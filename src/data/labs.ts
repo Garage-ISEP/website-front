188668612e28378f360e6929aad17a1d9c7204fb
MaxxxxxxLh <122477345+MaxxxxxxLh@users.noreply.github.com>
2023-08-20 15:09:54 +0200

    .

---
+++ src/data/labs.ts
@@ -0,0 +1,40 @@
+import { ModelLabs } from "@/components/labs/interfaces";
+
+export const datasLabs: ModelLabs[] = [
+    {
+      id: 0,
+      name: "Cyber",
+      image: "Cyber.png",
+      description: "Ransomware, botnets, ddos, phishing, les attaques cyber évoluent tous les jours dans notre monde numérique. Le cyberlab est là pour les comprendre et les éviter. "
+    },
+    {
+      id: 1,
+      name: "Blockchain",
+      image: "Blockchain.png",
+      description: "La blockchain est la technologie du moment. Entre crypto, NFT et finance décentralisée, les rouages du monde de demain sont étudiés dans le Blockchain Lab.       "
+    },
+    {
+      id: 2,
+      name: "Maker",
+      image: "MAKERV1.png",
+      description: "Dans un monde connecté, nous avons de plus en plus besoin de robotique. A l'aide de l'automatisation et de la conception, le Maker Lab bâtit le futur.            "
+    },
+    {
+      id: 3,
+      name: "Virtual",
+      image: "Virtual2.png",
+      description: "La VR est une technologie d'avenir, entre ces applications pour le monde du jeu vidéo ou encore les cinémas 5D, le Virtual Lab se penche sur le futur lointain.   "
+    },
+    {
+      id: 4,
+      name: "IA",
+      image: "IAV1.png",
+      description: "La connaissance sans la sagesse, c'est l'intelligence artificielle. L'IA Lab accumule les connaissances et les exploite pour créer les intelligences artificielles de demain."
+    },
+    {
+      id: 5,
+      name: "Coder",
+      image: "CODERV1.png",
+      description: "A l'aide d'outils modernes et très performants, le Coder Lab transforme les idées et les projets en solutions concrètes en les codant avec différents langages de programmation."
+    }
+  ]
